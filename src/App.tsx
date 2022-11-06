a899339edce5f19fe22b46106e5e98faad64a1b8
Alex Cândido <alex.candido.tec@gmail.com>
2022-11-06 19:52:20 -0300

    estrutura de routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,12 @@
-import * as Dialog from '@radix-ui/react-dialog';
-
-import { useEffect, useState } from 'react';
-import logoImg from './assets/logo-nlw-esports.svg';
-import CreateAdBanner from './components/CreateAdBanner';
-import { CreateAdModal } from './components/CreateAdModal';
-
-import GameBanner from './components/GameBanner';
-
-import { api } from './services/api';
-import './styles/main.css';
-
-interface GameProps {
-  id: string;
-  title: string;
-  bannerUrl: string;
-  _count: {
-    ads: number;
-  }
-}
+import { BrowserRouter } from 'react-router-dom';
+import Router from './routes';
 
 const App = () => {
-  const [games, setgames] = useState<GameProps[]>([]);
-
-  useEffect(() => {
-    api.get('/games').then((response) => {
-      setgames(response.data);
-    })
-  }, [])
-  
   return (
-    <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
-      <img src={logoImg} alt="" />
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+  );
+};
 
-      <h1 className="text-6xl text-white font-black mt-20">Seu <span className="text-transparent bg-nlw-gradient bg-clip-text">duo</span> está aqui</h1>
-
-      <div className="grid grid-cols-6 gap-6 mt-16">
-        {games.map((game, id) => (
-          <GameBanner 
-            key={id} 
-            bannerUrl={game.bannerUrl} 
-            title={game.title} 
-            adsCount={game._count.ads} 
-          />
-        ))}
-      </div>
-      
-      <Dialog.Root>
-        <CreateAdBanner />
-
-        <CreateAdModal />
-      </Dialog.Root>
-      
-    </div>
-  )
-}
-
-export default App
+export default App;
---
+++ src/routes/index.tsx
@@ -0,0 +1,14 @@
+import { Route, Routes } from 'react-router-dom';
+import Game from '../pages/Game';
+import Home from '../pages/Home';
+
+const Router = () => {
+  return (
+    <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/post/:id" element={<Game />} />
+    </Routes>
+  );
+};
+
+export default Router;
